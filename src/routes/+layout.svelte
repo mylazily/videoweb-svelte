34359d98ff46a7e3a6563dea06b991f063803118
lazily <mylazily@gmail.com>
2026-05-15 10:50:24 +0800

    Add src/routes/+layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import '../app.css';
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
+  import TabBar from './components/TabBar.svelte';
+  import { page } from '$app/stores';
+</script>
+
+<div class="min-h-screen flex flex-col">
+  <Header />
+  <main class="flex-1">
+    <slot />
+  </main>
+  <Footer />
+  <TabBar />
+</div>
